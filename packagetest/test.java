f52b023291a76e73ec090f43d0573c62b7d9719b
happyhancr <happyhancr@gmail.com>
2021-12-03 18:51:57 -0500

    Add variables

--- packagetest/test.java
+++ packagetest/test.java
@@ -2,6 +2,24 @@ package packagetest;
 
 public class test{
     private String str;
+    private String str2;
+    private String str3;
+
+    public String getStr3() {
+        return str3;
+    }
+
+    public void setStr3(String str3) {
+        this.str3 = str3;
+    }
+
+    public String getStr2() {
+        return str2;
+    }
+
+    public void setStr2(String str2) {
+        this.str2 = str2;
+    }
 
     public String getStr() {
         return str;
